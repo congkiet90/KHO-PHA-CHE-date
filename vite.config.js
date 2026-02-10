af158a3e94874c7fe4e5e3bc25d7fc752717385c
congkiet90@gmail.com <congkiet90@gmail.com>
2026-02-10 18:16:04 +0700

    fix: Reset configs to standard Vercel defaults

--- vite.config.js
+++ vite.config.js
@@ -4,19 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    define: {
-        // By default, Vite defines process.env.NODE_ENV, but some libs need it explicitly
-        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'production'),
-    },
-    build: {
-        rollupOptions: {
-            external: ['react', 'react-dom'],
-            output: {
-                globals: {
-                    react: 'React',
-                    'react-dom': 'ReactDOM',
-                },
-            },
-        },
-    },
 })
