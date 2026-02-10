481b52f7dee2238a2032d49e782a0309f6c00c25
congkiet90@gmail.com <congkiet90@gmail.com>
2026-02-10 18:13:28 +0700

    fix: Configure Vite to use external React from CDN

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,15 @@ export default defineConfig({
         // By default, Vite defines process.env.NODE_ENV, but some libs need it explicitly
         'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'production'),
     },
+    build: {
+        rollupOptions: {
+            external: ['react', 'react-dom'],
+            output: {
+                globals: {
+                    react: 'React',
+                    'react-dom': 'ReactDOM',
+                },
+            },
+        },
+    },
 })
