73638684972708d0ff8c613c747fda42fb0da06e
congkiet90@gmail.com <congkiet90@gmail.com>
2026-02-10 18:11:32 +0700

    fix: Nuclear option - Load React via CDN to guarantee global access

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    define: {
+        // By default, Vite defines process.env.NODE_ENV, but some libs need it explicitly
+        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'production'),
+    },
 })
